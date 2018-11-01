fe6db9125d85df467d3a07b36a9a6c4dacaff7d5
dayem10pearls <dayem.siddiqui@tenpearls.com>
2018-11-01 16:55:55 +0500

    Theme Assets Added

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueSocketio from 'vue-socket.io'
+import 'jquery/dist/jquery.min.js'
+import 'bootflat/css/bootstrap.min.css'
+import 'bootflat/css/site.min.css'
+import 'bootflat/js/site.min.js'
+import 'bootflat/js/application.js'
+import 'bootflat/js/bootstrap.min.js'
 
 Vue.config.productionTip = false
 Vue.use(VueSocketio, '/')
